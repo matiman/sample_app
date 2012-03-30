ebf4eab1b35ca07311c6b9605cf272d1a8902004
Mathias Abraham <bestmati1@gmail.com>
2012-03-30 14:10:58 -0700

    test started and i am on half way

--- app/controllers/static_pages_controller.rb~
+++ app/controllers/static_pages_controller.rb~
@@ -4,7 +4,4 @@ class StaticPagesController < ApplicationController
 
   def help
   end
-
-  def about
-  end
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help App'" do
+      visit '/static_pages/help'
+      page.should have_content('Help App')
+    end
+  end
+
+describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
+  
+
+end
+
