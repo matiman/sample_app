ccd1104f32027ebe8327e214f559896f57abfe62
Mathias Abraham <bestmati1@gmail.com>
2012-05-02 20:04:24 -0700

    exercise updates

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,6 +28,13 @@ describe "signup" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+	describe "error messages" do
+        	before { click_button submit }
+
+	        it { should have_selector('title', text: 'Sign up') }
+        	it { should have_content('error') }
+	end
+	    
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
@@ -36,14 +43,24 @@ describe "signup" do
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
+        fill_in "Email",        with: "user2@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm password", with: "foobar"
       end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+	describe "after saving the user" do
+        	before { click_button submit }
+	        let(:user) { User.find_by_email('user2@example.com') }
+
+        	it { should have_selector('title', text: user.name) }
+	        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      	end
+      	
+
     end
   end
 end
