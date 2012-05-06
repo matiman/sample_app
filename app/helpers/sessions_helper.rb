a8f5329ffb9f16607fb5da2def18ab4673231210
Mathias Abraham <bestmati1@gmail.com>
2012-05-06 01:00:34 -0700

    cookies replaced with session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
 
 	def sign_in(user)
-	    cookies.permanent[:remember_token] = user.remember_token
+	    session[:remember_token] = user.remember_token
 	    current_user = user
 	end
 
@@ -19,13 +19,13 @@ module SessionsHelper
 
 	def sign_out
 		current_user = nil
-		cookies.delete(:remember_token)
+		session[:remember_token]= nil
 	end
 
     private
 
     def user_from_remember_token
-      remember_token = cookies[:remember_token]
+      remember_token = session[:remember_token]
       User.find_by_remember_token(remember_token) unless remember_token.nil?
     end
 end
