1e53971d360ceb3bda68f09f12b39e913072f80c
Mathias Abraham <bestmati1@gmail.com>
2012-05-07 19:28:43 -0700

    updating-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,5 +20,20 @@ class UsersController < ApplicationController
 		
 	end
 
+	def edit
+		@user=User.find(params[:id])
+	end
+
+	def update
+		@user=User.find(params[:id])
+		if @user.update_attributes(params[:user])
+			flash[:success]= "Profile updated"
+			sign_in @user
+			redirect_to  @user
+		else
+			render 'edit'
+		end
+	end
+
 
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,14 +29,11 @@ describe "signin" do
 
 describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user }
 
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
@@ -52,4 +49,25 @@ describe "with valid information" do
 
   end
 end
-  
+
+describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    end
+  end
+
+
